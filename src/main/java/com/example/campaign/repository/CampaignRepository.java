62e0fe486b102ffced355b67540121bfbc12c4f6
ShashankV01 <vshashank001@gmail.com>
2026-09-16 21:08:28 +0530

    Update CampaignRepository.java

--- src/main/java/com/example/campaign/repository/CampaignRepository.java
+++ src/main/java/com/example/campaign/repository/CampaignRepository.java
@@ -2,6 +2,7 @@ package com.example.campaign.repository;
 
 import com.example.campaign.entity.Campaign;
 import com.example.campaign.entity.CampaignStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
